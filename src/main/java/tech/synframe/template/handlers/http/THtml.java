fd4f86f369396ecd8cb7f22ff52670d47eb8237a
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-04-24 00:53:59 -0700

    save - migrating to intellij

--- src/main/java/tech/synframe/template/handlers/http/THtml.java
+++ src/main/java/tech/synframe/template/handlers/http/THtml.java
@@ -6,13 +6,15 @@ import com.synload.framework.Log;
 import com.synload.framework.http.HttpRequest;
 import com.synload.framework.http.annotations.Get;
 import com.synload.framework.http.annotations.MimeType;
+import com.synload.framework.http.annotations.Post;
 
 /*
  * Class wrapping http response methods
  */
 public class THtml {
-	@Get(path = "/template") // HTTP get annotation ( registers as get request http://yoursite.com:8080/template )
-	@MimeType(type = "text/html") // returns OK from server and the mimetype content type
+	@Get("/template") // HTTP get annotation ( registers as get request http://yoursite.com:8080/template )
+	@MimeType("text/html") // returns OK from server and the mimetype content type
+	@Post("/retard_test")
 	public void gHtml(HttpRequest hr){
 		try {
 			hr.getResponse().getWriter().print("Module template works!");
